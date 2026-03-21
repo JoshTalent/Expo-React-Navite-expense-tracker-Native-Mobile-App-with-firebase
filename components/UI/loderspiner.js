9d165c537365deba872f2fd9d3e20428f007b705
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-03-21 21:35:50 +0200

    Fix formatting and syntax in Loderspiner component

--- components/UI/loderspiner.js
+++ components/UI/loderspiner.js
@@ -1,12 +1,15 @@
 import { ActivityIndicator,View ,StyleSheet} from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
+
 function Loderspiner(){
     return<View style={styles.container}>
         <ActivityIndicator size={"large"} color={"white"}/>
     </View>
 }
+
 export default Loderspiner;
+
 const styles=StyleSheet.create({
     container:{
         flex:1,
