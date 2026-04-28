913a6b4841b1ed12dd0583a0ab8d571bd7639ee9
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-28 21:14:58 +0200

    Clean up whitespace in ExpensesList.js
    Removed unnecessary whitespace in ExpensesList.js.

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,13 +1,10 @@
 import { FlatList } from 'react-native';
 
-
 import ExpenseItem from './ExpenseItem';
-
 
 function renderExpenseItem(itemData) {
   return <ExpenseItem {...itemData.item} />;
 }
-
 
 function ExpensesList({ expenses }) {
   return (
