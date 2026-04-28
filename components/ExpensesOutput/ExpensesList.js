1cf2f5a678e2bde3145500cf089d945d96e0a05a
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-28 21:14:33 +0200

    Add import statements for ExpenseItem and FlatList

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,10 +1,13 @@
 import { FlatList } from 'react-native';
 
+
 import ExpenseItem from './ExpenseItem';
+
 
 function renderExpenseItem(itemData) {
   return <ExpenseItem {...itemData.item} />;
 }
+
 
 function ExpensesList({ expenses }) {
   return (
