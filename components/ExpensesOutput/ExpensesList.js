ec42d67c054119763b955b35bb3c8006cbb58d51
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-28 21:13:31 +0200

    Clean up ExpensesList.js by removing blank lines
    Removed unnecessary blank lines in ExpensesList.js

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,11 +1,6 @@
 import { FlatList } from 'react-native';
 
-
-
-
 import ExpenseItem from './ExpenseItem';
-
-
 
 function renderExpenseItem(itemData) {
   return <ExpenseItem {...itemData.item} />;
