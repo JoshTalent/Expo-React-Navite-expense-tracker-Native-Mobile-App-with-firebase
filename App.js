5600ee370d1b70c3d909ba546e44c0183ed94adb
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-05-04 23:59:25 +0200

    Add import statement for ManageExpense component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
+
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
