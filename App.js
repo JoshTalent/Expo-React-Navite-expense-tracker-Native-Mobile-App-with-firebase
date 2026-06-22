915c6b015fb4c798f5c4e1886c93638a5a01cecb
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-06-22 13:02:42 +0200

    Update App.js

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { GlobalStyles } from './constants/styles';
 import IconButton from './components/UI/IconButton';
 import ExpensesContextProvider from './store/expenses-context';
 
+
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
